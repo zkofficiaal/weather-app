b64c5ba21552fef2561341dc4467786edd995016
Muhammad Zahid Khan <zkofficiaal@gmail.com>
2026-06-14 20:38:12 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+### UI
+
+<img width="250" alt="Screenshot 2023-11-18 at 11 07 35" src="https://github.com/martiniucanastasia/swiftui-weather-app/assets/86486215/7162bcff-3bc3-4654-ba93-f1b7c1de784c">
+<img width="250" alt="Screenshot 2023-11-18 at 11 07 35" src="https://github.com/martiniucanastasia/swiftui-weather-app/assets/86486215/125d46f3-4c12-4396-9697-67ee262e3da4">
---
+++ SwiftUI-WeatherApp/WeatherButton.swift
@@ -0,0 +1,27 @@
+//
+//  WeatherButton.swift
+//  SwiftUI-WeatherApp
+//
+//  Created by amartiniuc on 26.12.2023.
+//
+
+import SwiftUI
+
+struct WeatherButton: View {
+    
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor.gradient)
+            .foregroundColor(textColor)
+            .font(.system(size: 20,
+                          weight: .bold,
+                          design: .default))
+            .cornerRadius(10)
+    }
+}
+
